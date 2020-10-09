23f1e1fc876d489d73808e206e2233893daf39c7
JevgenijsKonevs <jevgenijskonevs@gmail.com>
2020-10-10 01:25:44 +0300

    row components improvements / banner component

--- src/Row.js
+++ src/Row.js
@@ -3,7 +3,7 @@ import axios from "./axios";
 import "./Row.css";
 const base_url = "https://image.tmdb.org/t/p/original/";
 
-export default function Row({ title, fetchUrl }) {
+export default function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
   useEffect(() => {
     async function fetchData() {
@@ -20,7 +20,8 @@ export default function Row({ title, fetchUrl }) {
       <div className="row__posters">
         {movies.map((movie) => (
           <img
-            className="row__poster"
+            key={movie.id}
+            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
             src={`${base_url}${movie.poster_path}`}
             alt={movie.name}
           />
